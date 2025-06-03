4e2632f60b92b9341f4bf6ee4ab2f3918eaffaf4
sudokrystian <48478528+sudokrystian@users.noreply.github.com>
2025-06-03 21:55:08 +0200

    Updating home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,27 +1,35 @@
 import "./home.scss";
-import { NavLink } from "react-router-dom";
-import cv from "../../assets/documents/CV.pdf";
 import MyParticles from "../../components/particles/MyParticles";
+import cv from "../../assets/documents/CV.pdf";
+import ghibli from "../../assets/my_pictures/new/ghibli.png";
+import ghibli2 from "../../assets/my_pictures/new/ghibli2.png";
+
+
 
 const Home = () => {
   return (
-    <div className="home-div">
+    <div className="home-root">
       <MyParticles />
-      <div className="welcome-div">
-        <h4>
-          Hi, I’m Krystian —  software engineer.
-          <br />
-          Building everything from backend beasts to algorithm speedruns,
-          interactive web, quirky games, and mind-bending AI.
-        </h4>
-        <a href={cv} target="_blank" rel="noreferrer">
-          <button className="cv-button">
-            Download my CV
-            <div className="download-icon">
-              <i className="fas fa-cloud-download-alt"></i>
-            </div>
-          </button>
-        </a>
+      <div className="welcome-section">
+        <div className="welcome-row">
+          <img src={ghibli} alt="Krystian meditating" className="welcome-img" />
+          <div className="welcome-text">
+            <h1>
+              Hi, I’m Krystian — software engineer.
+            </h1>
+            <p>
+              Building everything from backend beasts to algorithm speedruns, interactive web, quirky games, and mind-bending AI.
+            </p>
+            <a href={cv} target="_blank" rel="noreferrer">
+              <button className="cv-button">
+                Download my CV
+                <span className="download-icon">
+                  <i className="fas fa-cloud-download-alt"></i>
+                </span>
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
